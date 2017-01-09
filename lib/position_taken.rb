38f3635f4a806f39f9166e45f017d84c2729c1f3
Mila Hose <mila.hose@gmail.com>
2017-01-09 06:35:39 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,15 @@
 # code your #position_taken? method here!
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+
+
+
+# ttt-6-position-taken-4b-q-000
